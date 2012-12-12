5069aa8f79b6e30a38fc38aa6b317ab4ba9e1aa6
Thomas Levine <occurrence@thomaslevine.com>
2012-12-11 22:17:04 -0500

    aoeu

--- test_tick.py
+++ test_tick.py
@@ -18,21 +18,21 @@ def test_2s_round():
     n.assert_equal(o, e)
 
 def test_2s_ceil():
-    o = ticks(1, 3, ceil)
+    o = ticks(2, 3, ceil)
     e = [0, 2, 4]
     n.assert_equal(o, e)
 
 def test_5s_floor():
-    o = ticks(1, 3, floor)
-    e = [0, 2]
+    o = ticks(5, 22, floor)
+    e = [0, 5, 10, 15, 20]
     n.assert_equal(o, e)
 
 def test_5s_round():
-    o = ticks(2, 3, round)
-    e = [0, 2, 4]
+    o = ticks(5, 22, round)
+    e = [0, 5, 10, 15, 20, 25]
     n.assert_equal(o, e)
 
 def test_5s_ceil():
-    o = ticks(1, 3, ceil)
-    e = [0, 2, 4]
+    o = ticks(5, 22, ceil)
+    e = [0, 5, 10, 15, 20, 25]
     n.assert_equal(o, e)
--- tick.py
+++ tick.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 import math
 
-def tick_range_1s_round1(maxtick):
-    top = int(maxtick * 10)
-    inflatedrange = range(0, top, 10)
-    return [i/10 for i in inflatedrange]
-
-def tick_range_1s_round0(maxtick):
-    top = int(maxtick * 10 ** 0)
-    inflatedrange = range(0, top, 10)
-    return [i/10 for i in inflatedrange]
 
 if __name__ == '__main__':
     import sys
