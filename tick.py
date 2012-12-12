304dedf6c66e72a825586e07476aef5131a8ff80
Thomas Levine <occurrence@thomaslevine.com>
2012-12-11 22:32:15 -0500

    better

--- tick.py
+++ tick.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 import math
 
+def ticks(series, maxvalue, roundfn):
+    '''Use the `series` modules to generate a range for the `maxvalue`.
+    Use the `roundfn` for rounding.'''
+    imaxvalue = int(maxvalue)
+    remainder = float(imaxvalue % series)
+
+    # Approximate range aximum
+    floor_maxtick = int(series * (imaxvalue - remainder))
+
+    # Adjust by rounding
+    adjustment = roundfn(remainder / imaxvalue)
+
+    return range(0, int(floor_maxtick + adjustment), series)
+
+def ticks10(series, maxvalue, roundfn):
+    '''Run the `ticks` function on the maxvalue times ten, and return the
+    returned series times ten.'''
+    inflated = ticks(series, maxvalue * 10, roundfn)
+    return [i/10 for i in inflated]
 
 if __name__ == '__main__':
     import sys
