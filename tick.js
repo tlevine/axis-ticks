7202d8baa40c93a6fedbed157581abdf4a8606c1
Thomas Levine <occurrence@thomaslevine.com>
2012-12-11 21:26:37 -0500

    start a javascript tick function
    then decide that python is easier

---
+++ tick.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+tick = function(base, nticks, max){
+  // Place tick marks given a max tick number.
+  // The location is termined by an exponent base
+  // and by the number of ticks.
+  Math.pow
+}
