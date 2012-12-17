41826717740d1864dd1adfef1aeb5f740ca8f99d
Thomas Levine <occurrence@thomaslevine.com>
2012-12-16 21:46:37 -0500

    factoring

--- tick.hs
+++ tick.hs
@@ -2,15 +2,25 @@
 snap :: (RealFrac a, Integral b) => (a -> b) -> b -> b -> b
 snap direction base sharp = (direction ( (fromIntegral sharp) / (fromIntegral base) ) ) * base
 
+-- Convenience functions
+floorBase = snap floor
+roundBase = snap round
+ceilingBase = snap ceiling
+
 -- Convert a number into a significand and a power of 10.
-factor :: (Floating a, RealFrac a) => a-> (a, a)
-factor number = (significand, magnitude)
+factorBase :: (Floating a, RealFrac a) => a -> a -> (a, a)
+factorBase base number = (significand * base, magnitude / base)
   where
     magnitude = 10 ^ (floor ( logBase 10 number ))
     significand = number / magnitude
 
+-- Convenience functions
+factor1 = factorBase 1
+factor10 = factorBase 10
+
 main :: IO ()
 main = do
-  --let foo = snap ceiling 3 7
-  let foo = factor 234
+  let foo = floorBase 3 7
+  let bar = factor10 234
   putStrLn $ show foo
+  putStrLn $ show bar
