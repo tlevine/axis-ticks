76dc933e9948073ba6dbb136dd0e7b4133c865cb
Thomas Levine <occurrence@thomaslevine.com>
2012-12-16 21:18:45 -0500

    Remove junk.

--- tick.hs
+++ tick.hs
@@ -1,17 +1,8 @@
 -- Given a round function, a base to round to, round a sharp value.
--- snap :: Integral a => (b -> a) -> a -> a -> a
--- snap direction base sharp = (direction ( sharp / base )) * base
-
 snap :: (RealFrac a, Integral b) => (a -> b) -> b -> b -> b
-snap direction base sharp = (direction ( fromIntegral ( sharp `div` base ))) * base
-
-snapTest :: Int -> Int -> Int
-snapTest 3 5 = 6
-snapTest 5 6 = 5
-snapTest 3 10 = 0
-snapTest _ _ = 0
+snap direction base sharp = (direction ( (fromIntegral sharp) / (fromIntegral base) ) ) * base
 
 main :: IO ()
 main = do
-  let foo = snap ceiling 3 5
+  let foo = snap ceiling 3 7
   putStrLn $ show foo
