79720bf32834fced39da5d0cab5d4df77e3ef794
Thomas Levine <occurrence@thomaslevine.com>
2012-12-16 21:22:41 -0500

    Remove junk.

--- tick.hs
+++ tick.hs
@@ -2,7 +2,12 @@
 snap :: (RealFrac a, Integral b) => (a -> b) -> b -> b -> b
 snap direction base sharp = (direction ( (fromIntegral sharp) / (fromIntegral base) ) ) * base
 
+-- Convert a number into a significand and a power of 10.
+factor (Num a, Integral b) => a -> (b, b)
+factor _ = (3, 1000)
+
 main :: IO ()
 main = do
-  let foo = snap ceiling 3 7
+  --let foo = snap ceiling 3 7
+  let foo = factor 234
   putStrLn $ show foo
