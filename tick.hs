5b840a4c8909b41d7e92ed4d6731113199bc31bf
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 00:25:02 -0500

    print intervalwq

--- tick.hs
+++ tick.hs
@@ -1,20 +1,24 @@
--- The interval in scientific notation
-type Interval = (Int, Int)
+-- Traverse intervals in scientific notation
+type Interval = (Float, Int)
 
 prevInterval :: Interval -> Interval
 prevInterval (significand, magnitude)
-  | significand == 5 = (2, magnitude)
-  | significand == 2 = (1, magnitude)
-  | significand == 1 = (5, magnitude - 1)
+  | significand > 5 = (2, magnitude)
+  | significand > 2 = (1, magnitude)
+  | significand > 1 = (5, magnitude - 1)
 
 nextInterval :: Interval -> Interval
 nextInterval (significand, magnitude)
-  | significand == 5 = (1, magnitude + 1)
-  | significand == 2 = (5, magnitude)
-  | significand == 1 = (2, magnitude)
+  | significand > 5 = (1, magnitude + 1)
+  | significand > 2 = (5, magnitude)
+  | significand > 1 = (2, magnitude)
 
+-- The interval as one number
 showInterval :: Interval -> Float
-showInterval (significand, magnitude) = (fromIntegral significand) * 10 ^^ magnitude
+showInterval (significand, magnitude) = (significand) * 10 ^^ magnitude
+
+-- The ideal interval if humans could read weird numbers
+idealInterval :: Float -> Interval
 
 main = do
   putStrLn $ show $ showInterval $ prevInterval (5, 2)
