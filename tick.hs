df6a997ef9f52acfd486d30ec1783ed9cbda55ac
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 01:11:30 -0500

    oops

--- tick.hs
+++ tick.hs
@@ -62,4 +62,6 @@ main = do
   putStrLn $ show $ fromInterval $ nextInterval $ idealInterval 8.1234 4
   putStrLn $ show $ fromInterval $ nextInterval $ idealInterval 324 8
   putStrLn $ show $ ticks' [0] 324 50
-  putStrLn $ show $ ticks 0 324 8
+  tryMany = putStrLn $ show $ ticks 0 324 
+  tryMany 8
+  tryMany 9
