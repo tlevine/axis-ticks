0ce309d513f8aec106d7dd202c14a2ab43d82f9b
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 00:21:35 -0500

    print intervalwq

--- tick.hs
+++ tick.hs
@@ -7,5 +7,14 @@ prevInterval (significand, magnitude)
   | significand == 2 = (1, magnitude)
   | significand == 1 = (5, magnitude - 1)
 
+nextInterval :: Interval -> Interval
+nextInterval (significand, magnitude)
+  | significand == 5 = (1, magnitude + 1)
+  | significand == 2 = (5, magnitude)
+  | significand == 1 = (2, magnitude)
+
+showInterval :: Interval -> Float
+showInterval (significand, magnitude) = (fromIntegral significand) * 10 ^^ magnitude
+
 main = do
-  putStrLn $ show $ prevInterval (5, -3)
+  putStrLn $ show $ showInterval $ prevInterval (5, 2)
