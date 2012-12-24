7f87759fff43321ff1693c74b819f4a96e175915
Thomas Levine <occurrence@thomaslevine.com>
2012-12-24 01:33:35 -0500

    aoeu

--- tick.hs
+++ tick.hs
@@ -46,22 +46,31 @@ ticks dataMin dataMax nticks = ticks' [dataMin] dataMax (fromInterval next)
 ticksZero :: Float -> Int -> [Float]
 --ticks dataMin dataMax nticks = ticks' [(fromIntegral (floor dataMin))] dataMax (fromInterval prev)
 ticksZero dataMax nticks
-  | prevError < nextError = ticks' [0] dataMax prev
+  | prevError <= nextError = ticks' [0] dataMax prev
   | otherwise = ticks' [0] dataMax next
     where
-      ideal = idealInterval (dataMax - 0) nticks
+      nsteps = max 1 $ nticks - 1
+      ideal = idealInterval (dataMax - 0) nsteps
+      nsteps' = (fromIntegral nsteps)
       prev = fromInterval $ prevInterval ideal
       next = fromInterval $ nextInterval ideal
-      prevError = abs ((prev * (fromIntegral nticks)) - dataMax)
-      nextError = abs ((next * (fromIntegral nticks)) - dataMax)
+      prevError = abs ((prev * nsteps') - dataMax)
+      nextError = abs ((next * nsteps') - dataMax)
 
-main = do
+test = do
   putStrLn $ show $ fromInterval $ prevInterval (6, 2)
   putStrLn $ show $ fromInterval $ idealInterval 8.1234 4
   putStrLn $ show $ fromInterval $ prevInterval $ idealInterval 8.1234 4
   putStrLn $ show $ fromInterval $ nextInterval $ idealInterval 8.1234 4
   putStrLn $ show $ fromInterval $ nextInterval $ idealInterval 324 8
   putStrLn $ show $ ticks' [0] 324 50
-  putStrLn $ show $ ticks 0 324 7
-  putStrLn $ show $ ticks 0 324 8
-  putStrLn $ show $ ticks 0 324 9
+
+main = do
+--putStrLn $ show $ [1..20]
+--putStrLn $ show $ fmap (\ x -> length (ticks 0 324 x)) [1..20]
+--putStrLn "The sequences:"
+--putStrLn $ show $ fmap (ticks 0 324) [1..20]
+
+  putStrLn $ show $ [1..20]
+  putStrLn $ show $ fmap (\ x -> length (ticksZero 324 x)) [1..20]
+  putStrLn $ show $ fmap (ticksZero 324) [1..20]
